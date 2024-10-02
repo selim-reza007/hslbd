c9d8abb26bff9b4424a1925d3f28e1a9c975855f
Selim Reza <selim.reza.dia@gmail.com>
2024-10-02 13:15:38 +0600

    Added change password features

--- hslbd/urls.py
+++ hslbd/urls.py
@@ -1,28 +1,12 @@
-"""
-URL configuration for hslbd project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', views.homeView, name='home'), #loads home page
+    path('', views.homeView, name='home'),  # Loads home page
     path('dashboard/', views.dashboardView, name='dashboard'),
     path('products/', include('products.urls')),
     path('gallery/', include('gallery.urls')),
@@ -31,6 +15,5 @@ urlpatterns = [
     path('brand/', include('brand.urls')),
     path('admin/', include('user.urls')),
 ]
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'user'
 
 urlpatterns = [
     path('', views.loginView, name='user-login'),
     path('logout/', views.logoutView, name='user-logout'),
+    path('change-password/', 
+         auth_views.PasswordChangeView.as_view(template_name='user/change_password.html', success_url=reverse_lazy('user:password_change_done')), name='change_password'),
+    path('change-password/done/', 
+         auth_views.PasswordChangeDoneView.as_view(template_name='user/change_password_done.html'), name='password_change_done'),
 ]
