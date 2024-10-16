b273285a1e16568aaac371f44ac902f9220ff438
Selim Reza <selim.reza.dia@gmail.com>
2024-10-16 11:45:42 +0600

    Authorization to category is implemented plus success messages added for edit and delete

--- category/views.py
+++ category/views.py
@@ -3,15 +3,17 @@ from .forms import CreateCategory
 from django.db import IntegrityError, DatabaseError
 from products.models import Category
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 #listing out all created categories
+@login_required(login_url='/admin/')
 def allCategoriesView(request):
     data = Category.objects.all()
     return render(request, 'dashboard/all-category.html', { 'categories' : data })
 
 #Creating new category
+@login_required(login_url='/admin/')
 def addCategoryView(request):
     if request.method == "POST":
         form = CreateCategory(request.POST)
@@ -28,6 +30,7 @@ def addCategoryView(request):
 
 
 #edit category
+@login_required(login_url='/admin/')
 def editCategoryView(request, slug):
     datum = get_object_or_404(Category, slug=slug)
     if request.method == "POST":
@@ -35,6 +38,7 @@ def editCategoryView(request, slug):
         if form.is_valid:
             try:
                 form.save()
+                messages.success(request, "Category updated.")
                 return redirect('category:all-categories')
             except ImportError:
                 return render(request, 'Error.html', { 'errorMsg' : 'Integrity error occured!' })
@@ -43,11 +47,13 @@ def editCategoryView(request, slug):
     return render(request, 'dashboard/add-category.html', { 'form' : form })
 
 #delete category
+@login_required(login_url='/admin/')
 def deleteCategoryView(request, slug):
     datum = get_object_or_404(Category, slug=slug)
     if request.method == "POST":
         try:
             datum.delete()
+            messages.success(request, "Category Deleted.")
             return redirect('category:all-categories')
         except DatabaseError:
             return render(request, 'Error.html', { 'errorMsg' : 'Database error occured!' })
